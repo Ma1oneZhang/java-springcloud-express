f77e765b948a7f0f9ee0febe440ec8ec6a1abb36
gouqichun <525458513@qq.com>
2022-12-19 11:27:59 +0800

    build(init eurekaServer): init eurekaServer
    init eurekaServer

---
+++ eurekaServer/src/main/java/com/express/EurekaServerApplication.java
@@ -0,0 +1,11 @@
+package com.express;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
