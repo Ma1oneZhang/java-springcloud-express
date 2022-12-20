eaaa605e30e7ce1ab33ebefe78e72fca43f49b0c
gouqichun <525458513@qq.com>
2022-12-20 12:47:32 +0800

    build(init configServer): init configServer
    init configServer

--- configServer/src/main/java/com/express/ConfigServerApplication.java
+++ configServer/src/main/java/com/express/ConfigServerApplication.java
@@ -2,13 +2,12 @@ package com.express;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class})
-@EnableEurekaServer
-public class EurekaServerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigServerApplication.class,args);
     }
 }
