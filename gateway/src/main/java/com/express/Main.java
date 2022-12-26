4542fbf0497aa5be41e37b839095ded83131a849
zzy <202019060120@stu.cdut.edu.cn>
2022-12-26 15:28:30 +0800

    build(init gateway server && init eureka server)

--- gateway/src/main/java/com/express/Main.java
+++ gateway/src/main/java/com/express/Main.java
@@ -1,11 +1,12 @@
 package com.express;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
